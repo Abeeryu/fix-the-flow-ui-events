b0f7ad531bf2e047009c8e6306cf685e0ce7c5c5
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2023-12-14 00:43:26 +0100

    changes

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,166 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+let knop = document.querySelector("a:nth-of-type(1)");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+knop.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
+
+
+
+
+
+let password = document.querySelector("a:nth-of-type(2)");
+
+password.addEventListener("focus", (event) => {
+  event.target.style.background = "pink";
+});
+
+password.addEventListener("blur", (event) => {
+  event.target.style.background = "";
+});
+
+
+
+
+
+function zoom(event) {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // Apply scale transform
+  el.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const el = document.querySelector("a:nth-of-type(3)");
+el.onwheel = zoom;
+
+
+
+
+
+
+let test = document.querySelector("a:nth-of-type(4)");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener(
+  "mouseenter",
+  (event) => {
+    // highlight the mouseenter target
+    event.target.style.color = "purple";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false,
+);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false,
+);
+
+
+
+
+
+
+
+let frontend = document.querySelector('a:nth-of-type(5)')
+
+frontend.addEventListener('dblclick' ,
+
+function () {
+  window.open('https://www.pexels.com/search/hd/',
+      'width=400,height=400 top=200,left=600');
+});
+
+
+
+
+
+
+
+
+let evenement = document.querySelector('a:nth-of-type(6)')
+
+evenement.addEventListener("click", moveWithMouse, false); 
+
+window.onmousemove = function (e) { 
+  var x = e.pageX,
+   y = e.pageY;
+  evenement.style.top = (y) + 'px';
+  evenement.style.left = (x) + 'px';
+};
+
+
+function moveWithMouse() {
+  this.classList.add('absolute');
+  this.innerHTML = "states";
+  console.log('hi')
+}
+
+
+
+
+
+
+let design = document.querySelector('a:nth-of-type(7)')
+
+design.addEventListener('click', groter)
+
+function random(number) {
+  return Math.floor(Math.random() * number); 
+}
+
+function randomDeg() {
+  return `${random(360)}deg`; 
+}
+
+function groter() {
+  this.style.rotate = randomDeg(); 
+  console.log(randomDeg())
+}
+
+
+
+
+let button = document.querySelector('a:nth-of-type(8)')
+// Selects the FIRST occurance of <button>;
+let element = document.querySelector("div");
+
+button.addEventListener("click", function() {
+  element.classList.toggle("yay");
+});
+
+
+
+
+
+
